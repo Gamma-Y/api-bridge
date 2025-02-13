669b83f54e424b30f22caece2fc023fe4cfbede6
Evgeny Smirnov <HoUsToN.97@mail.ru>
2025-02-13 22:50:34 +0500

    init project

---
+++ src/main/java/ru/pet/project/api_bridge/ApiBridgeApplication.java
@@ -0,0 +1,13 @@
+package ru.pet.project.api_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pet/project/api_bridge/ApiBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pet.project.api_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
